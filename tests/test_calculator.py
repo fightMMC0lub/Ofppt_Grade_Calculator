c8a7953e7622ea10152e97c6d3dbb285fd647ee1
fouad azahaf <getshop463@gmail.com>
2025-07-20 04:01:07 +0100

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,1 +1,14 @@
+import unittest
+from app.calculator import calc_comm, calc_final
 
+class TestOFPPTCalculator(unittest.TestCase):
+    def test_calc_comm(self):
+        self.assertAlmostEqual(calc_comm(12, 14, 10), 12.67, places=2)
+
+    def test_calc_final(self):
+        moyenne = calc_final(10, 12, 11, 13, 14, 13, 12)
+        self.assertTrue(0 <= moyenne <= 20)
+        self.assertAlmostEqual(moyenne, 12.83, places=2)
+
+if __name__ == "__main__":
+    unittest.main()
