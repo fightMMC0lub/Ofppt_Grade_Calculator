52ac474fe81a4bb32e6d2a7d489b7c43940ac6a8
fouad azahaf <getshop463@gmail.com>
2025-07-20 03:56:08 +0100

    Update calculator.py

--- app/calculator.py
+++ app/calculator.py
@@ -1,1 +1,16 @@
+def calc_comm(french: float, arabic: float, english: float) -> float:
+  
+    return (2 * french + arabic + english) / 3
 
+def calc_final(
+    passage: float,
+    modules: float,
+    theory: float,
+    practice: float,
+    french: float,
+    arabic: float,
+    english: float
+) -> float:
+
+    comm = calc_comm(french, arabic, english)
+    return (passage + 2 * modules + 2 * theory + 4 * practice + comm) / 10
